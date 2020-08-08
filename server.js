8c19caaa4d19d6a4efd519db02a0b9dc341fe0c6
psn221b <prathik.s.nayak@gmail.com>
2020-08-08 18:42:35 +0530

    Implement room

--- server.js
+++ server.js
@@ -1,17 +1,42 @@
 const server = require('http').createServer();
-const io = module.exports.io = require('socket.io')(server);
+const io = require('socket.io')(server);
 
+/*
+  users code
+  0 - no user present
+  1 - only white present
+  2 - bothw white and black present
+  3 - only black present
+*/
+var rooms = {
+  111:{
+    state:null,
+    users:0
+  }
+};
 const PORT = 3001;
 user = 0; 
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  
-  if(user<3)
-  {
-    user+=1;
-    socket.emit('user',user);
-  }
+  // console.log('a user connected');
+  socket.on('send roomId',(roomId,state)=>{
+    if(roomId in rooms)
+    {
+      console.log(roomId+' room present');
+      if(user===0) 
+      {
+        rooms[roomId] = {...rooms[roomId],state}; 
+        user=1;
+        socket.emit('user',1,rooms[roomId].state);
+      }
+      else if(user===1)
+      {
+        user=2;
+        socket.emit('user',2,rooms[roomId].state);
+        io.emit('second joined');
+      } 
+    }
+  });  
   
   // When some move is made
   socket.on('move made',(state)=>{
@@ -21,6 +46,7 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
+
   });
 });  
 
